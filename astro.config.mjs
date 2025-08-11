71a87eaadab1031100a3766d0c668ab5b532be82
Breno <brenosalcantara@hotmail.com>
2025-08-11 13:26:33 -0300

    feat: reconfiguração da base url no config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: "https://brenoalcantara.github.io",
+  base: '/brenoalcantara.dev.br',
   integrations: [tailwind(), icon({customIconsDir: 'src/icons',}), mdx(), sitemap()]
 });
