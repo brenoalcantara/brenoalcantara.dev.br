81392f8f4512ae2cf1ab6b22275eb53f0575f63e
Breno <brenosalcantara@hotmail.com>
2026-04-29 19:02:40 -0300

    feat: alterar path das imagens e customizar config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,19 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blogsCollection = defineCollection({
-    schema: z.object({
+  schema: ({ image }) =>
+    z.object({
       title: z.string(),
       description: z.string(),
       // datetime: z.string(),
       datetime: z.coerce.date(),
-      image: z.string().optional(),
+      // image: z.string().optional(),
+      image: image().optional(),
       charset: z.string().default("UTF-8"),
-      keywords: z.string().optional(), 
+      keywords: z.string().optional(),
     }),
-
 });
 
 export const collections = {
-  'blogs': blogsCollection,
+  blogs: blogsCollection,
 };
